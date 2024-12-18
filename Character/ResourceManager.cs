b3d754e9337e644c0a320aec61cb8737d1478316
l3miage-bordesan <batiste.bordes@gmail.com>
2024-12-18 09:45:53 +0100

    Déplacement de QuantityBar dans la package UI

--- Character/ResourceManager.cs
+++ Character/ResourceManager.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Xml.Serialization;
 using Microsoft.Xna.Framework;
+using JeuVideo.UI;
 
 namespace JeuVideo.Character;
 
--- UI/QuantityBar.cs
+++ UI/QuantityBar.cs
@@ -1,7 +1,7 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace JeuVideo;
+namespace JeuVideo.UI;
 
 public class QuantityBar
 {
